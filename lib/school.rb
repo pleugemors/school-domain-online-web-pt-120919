9bb254982341027b3471035b0ea809e9af1344ce
Paul Leugemors <pleugemors@yahoo.com>
2019-12-10 21:16:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,22 +1,3 @@
 # code here!
 class School 
 
-  def initialize(name)
-    @name = name
-    roster ={}
-  end
-  
-  def roster
-    roster
-  end
-  
-  def add_student(name, grade)
-      if roster.empty?
-        print roster
-        roster[grade] = []
-        print roster
-        roster[grade] << name 
-        puts roster
-      end
-  end
-end
