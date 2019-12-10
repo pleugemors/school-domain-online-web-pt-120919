00a4a64410bba329a132d8a320a094fedcfbcda5
Paul Leugemors <pleugemors@yahoo.com>
2019-12-10 21:34:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,12 +8,12 @@ class School
   attr_reader :roster 
   
   def add_student(name, grade)
-    if @roster.empty?
+    if @roster.key?(grade)
       @roster[grade] = []
       @roster[grade] << name 
-    elsif !@roster.key?(grade)
-      roster[grade] = []
-      roster[grade] << name
+    #elsif !@roster.key?(grade)
+     # roster[grade] = []
+      #roster[grade] << name
     else 
       @roster[grade] << name 
     end
