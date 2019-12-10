f32fd50f41bd2cf1209c636a112069c6c849ddc2
Paul Leugemors <pleugemors@yahoo.com>
2019-12-10 21:25:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,7 @@ class School
     if @roster.empty?
       @roster[grade] = []
       @roster[grade] << name 
-    elsif @roster[grade].value?
+    elsif @roster[grade].size == nil
       roster[grade] = []
       roster[grade] << name
     else 
